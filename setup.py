63ade4d2fc8d8569d0336211edc139889320ff65
William McCorkindale <wjm41@cam.ac.uk>
2022-03-22 15:43:41 +0000

    initialise env & setup

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
+import os
+import codecs
 from setuptools import find_packages, setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="fresco",
-    version="0.1.0",
+    version=get_version("fresco/__init__.py"),
     description="Code for performing virtual screening by applying unsupervised machine learning on crystal structures of fragment-protein complexes.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/wjm41/frag-pcore-screen",
-    packages=find_packages(),
+    packages=['fresco'],
     author="W. McCorkindale",
     license="MIT License"
 )
