ab95668396633f60aa2b2191746647a83995578d
wjm41 <wjm41@cam.ac.uk>
2022-06-08 11:53:06 +0100

    added weight argument to featurisation function

--- fresco/featurise.py
+++ fresco/featurise.py
@@ -1,6 +1,7 @@
 import os
 import math
 import logging
+from collections import Counter
 from typing import List
 
 import numpy as np
@@ -82,10 +83,24 @@ def return_pcore_dataframe_from_single_rdkit_molecule(mol: Mol, mol_id: int = 0,
         return pd.concat(df_of_pcores_for_this_mol)
 
 
-def return_pcore_dataframe_for_list_of_mols(mols: List[Mol], pcores_of_interest: List = None, featFactory=None) -> pd.DataFrame:
+def calculate_frequency_weights_for_duplicate_fragments(smiles_list: List[str]):
+    
+    frequency_counts_per_smiles = Counter(smiles_list)
+
+    weights = [1/frequency_counts_per_smiles[smi] for smi in smiles_list]
+    
+    return weights
+    
+
+def return_pcore_dataframe_for_list_of_mols(mols: List[Mol], smiles_list: List[str] = None, pcores_of_interest: List = None, featFactory=None) -> pd.DataFrame:
     """
     retun dictionary of numpy arrays containing (x,y,z) of pharmacophore coordinates (averaged over atoms)
     """
+
+    if smiles_list is None:
+        smiles_list = [MolToSmiles(mol) for mol in mols]
+    else:
+        assert len(smiles_list) == len(mols)
 
     if pcores_of_interest is None:
         pcores_of_interest = return_default_pharmacophores()
@@ -94,12 +109,14 @@ def return_pcore_dataframe_for_list_of_mols(mols: List[Mol], pcores_of_interest:
         featFactory = return_rdkit_feat_factory()
 
     dfs_for_this_list_of_mols = []
-
+    weights = calculate_frequency_weights_for_duplicate_fragments(smiles_list)
+    
     for mol_id, mol in tqdm(enumerate(mols), total=len(mols)):
 
         df_for_this_mol = return_pcore_dataframe_from_single_rdkit_molecule(
             mol, mol_id=mol_id, pcores_of_interest=pcores_of_interest, featFactory=featFactory)
-
+        df_for_this_mol['weight'] = weights[mol_id]
+        
         if df_for_this_mol is not None:
             dfs_for_this_list_of_mols.append(df_for_this_mol)
 
@@ -118,11 +135,13 @@ def calculate_pairwise_distances_between_pharmacophores_for_fragment_ensemble(df
     '''
 
     df_pcore_a = df_of_frag_ensemble_pcores.query('pcore == @pcore_a')
-    id_a = df_pcore_a['mol_id']
+    smiles_a = df_pcore_a['smiles']
+    weights_a = df_pcore_a['weight'].to_numpy()
     coords_a = df_pcore_a[['coord_x', 'coord_y', 'coord_z']].to_numpy()
 
     df_pcore_b = df_of_frag_ensemble_pcores.query('pcore == @pcore_b')
-    id_b = df_pcore_b['mol_id']
+    smiles_b = df_pcore_b['smiles']
+    weights_b = df_pcore_b['weight'].to_numpy()
     coords_b = df_pcore_b[['coord_x', 'coord_y', 'coord_z']].to_numpy()
 
     if len(df_pcore_a) == 0:
@@ -133,26 +152,39 @@ def calculate_pairwise_distances_between_pharmacophores_for_fragment_ensemble(df
             f'No pharmacophores found for pharmacophore {pcore_b} !')
 
     distances_for_all_pairs = []
-
-    for id_of_frag_with_pcore_a in id_a.unique().astype(int):
-        xyz_i = coords_a[id_a == id_of_frag_with_pcore_a, :]  # DOUBLE COUNTING
-
+    weights_for_all_pairs = []
+    
+    for smiles_of_frag_with_pcore_a in smiles_a.unique().astype(int):
+        from_the_same_fragment = smiles_a == smiles_of_frag_with_pcore_a
+        
+        xyz_i = coords_a[from_the_same_fragment, :]  # DOUBLE COUNTING
+        w_i = weights_a[from_the_same_fragment, :]
+        
         # Don't count distances within the same fragment
-        xyz_j = coords_b[id_b != id_of_frag_with_pcore_a, :]
+        from_a_different_fragment = smiles_b != smiles_of_frag_with_pcore_a
+        xyz_j = coords_b[from_a_different_fragment, :]
+        w_j = weights_b[from_a_different_fragment, :]
 
         if len(xyz_j) > 0:
             delta_coordinates_between_pcores = xyz_i[:, np.newaxis] - xyz_j
-
+            
             distances_for_this_pair = np.linalg.norm(
                 delta_coordinates_between_pcores, axis=2)
 
             distances_for_this_pair = distances_for_this_pair.flatten()
+            
+            weights_for_this_pair = w_i[:, np.newaxis] * w_j
+            weights_for_this_pair = weights_for_this_pair.flatten()
+            
+            assert distances_for_this_pair.shape == weights_for_this_pair.shape
+            
+            distances_for_all_pairs.append(distances_for_this_pair)
+            weights_for_all_pairs.append(weights_for_this_pair)
+            
         else:
             logging.warning(
-                f'Only intra-fragment distance found for {id_of_frag_with_pcore_a} with pharmacophore {pcore_a}!')
-
-        distances_for_all_pairs.append(distances_for_this_pair)
-
+                f'Only intra-fragment distance found for {smiles_of_frag_with_pcore_a} with pharmacophore {pcore_a}!')
+        
     if len(distances_for_all_pairs) == 0:
         distances_for_all_pairs = [np.array([])]
     return np.hstack(distances_for_all_pairs)
